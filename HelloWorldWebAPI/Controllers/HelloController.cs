f6398400866b9720e5abea90c594194941159777
JakeWalsh69 <jrwalsh1997@gmail.com>
2019-02-21 13:08:10 +0000

    First Commit

---
+++ HelloWorldWebAPI/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+using HelloWorldWebAPI.Models;
+
+namespace HelloWorldWebAPI.Controllers
+{
+    [Route("api/Hello")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+
+        // GET /api/hello/Gary
+        [HttpGet("{name:alpha}")]
+        public ActionResult<Greeting> Get(String name)
+        {
+            return Ok(new Greeting() { Message = "Hello Word", To = name });      // 200 OK
+        }
+        
+    }
+}
